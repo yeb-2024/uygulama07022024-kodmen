91111fda9c8a188e1b7e47f3e216571e2cb48fab
bedirhan ergün <bedirhan1029@gmail.com>
2025-02-07 18:29:19 +0300

    Anasaya oluşturuldu

---
+++ Uygulama/src/main/java/org/example/Anasayfa.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Anasayfa {
+
+    JFrame frame = new JFrame();
+
+    public Anasayfa() {
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Anasayfa");
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
